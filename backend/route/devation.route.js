d9b68fcd22f06d864ae242a52747d021896c04ad
Rishu raj <21je0769@iitism.ac.in>
2025-01-11 11:08:45 +0530

    rote change as per demand

--- backend/route/devation.route.js
+++ backend/route/devation.route.js
@@ -3,6 +3,6 @@ import express from "express";
 import { getStandardDeviation } from "../controllers/standardDev.controllers.js";
 
 const router = express.Router();
-router.route("/sd/:coinId").get(getStandardDeviation);
+router.route("/deviation/:coinId").get(getStandardDeviation);
 
 export default router;
--- backend/route/stat.route.js
+++ backend/route/stat.route.js
@@ -3,6 +3,6 @@ import express from "express";
 import { getCryptoData } from "../controllers/stat.controllers.js";
 
 const router = express.Router();
-router.route("/stat/:coin").get(getCryptoData);
+router.route("/stats/:coin").get(getCryptoData);
 
 export default router;
